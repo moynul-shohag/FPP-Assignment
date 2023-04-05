c34f75205b37d3da64395e6732dd1abc42450656
Moynul <mail2moynul@gmail.com>
2023-04-05 14:18:48 -0500

    done

--- src/prog1_2/Main.java
+++ src/prog1_2/Main.java
@@ -5,5 +5,7 @@ public class Main {
         String expr="Hi \uD83D\uDC4B, my name is \u09AE\u0988\u09A8\u09C1\u09B2";
         System.out.println(expr);
         System.out.println(expr.length());
+
+        //The length of "Expr" string is 23. though there is too many character but each code unit("\u09AE") count as single character.
     }
 }
